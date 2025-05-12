c45bda0ceb090c56e04167b59973d1d6ffea5d6f
OmadAzam <138263140+OmadAzam@users.noreply.github.com>
2025-05-11 23:22:15 -0600

    testing basic stuff

---
+++ basics.go/constants.go
@@ -0,0 +1,16 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.82
+
+func main() {
+	const days int = 7
+
+	const (
+		MONDAY        = 1
+		TUESDAY       = 2
+		WEDNESDAY     = 3
+		THURSDAY  int = 4
+	)
+
+}
